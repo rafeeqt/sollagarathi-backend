839f5b27aecd4bd882302b6cecaa0d95e7d29325
Solagarathi <119445046+rafeeqt@users.noreply.github.com>
2026-01-17 23:14:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
-host: process.env.DB_HOST,
-user: process.env.DB_USER,
-password: process.env.DB_PASSWORD,
-database: process.env.DB_NAME,
-port: process.env.DB_PORT,
-ssl: { rejectUnauthorized: false }
- });
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
 
 app.get("/", (req, res) => {
   res.send("Sollagarathi Backend Running");
