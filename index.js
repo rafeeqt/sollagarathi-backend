cce0f54441256aa1fe066849e591cb6976a97572
rafeeqt <119445046+rafeeqt@users.noreply.github.com>
2026-01-17 13:11:07 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+import pkg from "pg";
+
+const { Pool } = pkg;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false }
+});
+
+app.get("/", (req, res) => {
+  res.send("Sollagarathi Backend Running");
+});
+
+app.get("/search/:word", async (req, res) => {
+  const { word } = req.params;
+
+  await pool.query(
+    "INSERT INTO search_history(tamil_word) VALUES($1)",
+    [word]
+  );
+
+  const result = await pool.query(
+    "SELECT * FROM words WHERE tamil_word=$1",
+    [word]
+  );
+
+  res.json(result.rows);
+});
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Server started")
+);
